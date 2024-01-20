449cf3aff5cb2e26ff4759eb45ae479bab273217
Sumeet Mishra <116353839+sumeetmishra809@users.noreply.github.com>
2024-01-20 23:57:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = " "; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
